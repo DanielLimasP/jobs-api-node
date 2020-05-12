ef2c33f684dac688e24407c2e2268e2d552d94c5
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-05-12 17:39:55 -0600

    Branching to try fix the User endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -12,14 +12,13 @@ router.post('/uploadphoto', AuthController.uploadProfilePhoto)
 
 router.post('/uploads3', AuthController.uploadToS3)
 
+router.post('/register', AuthController.signUpUser)
 
-router.post('/register', AuthController.signUpUser);
+router.post('/login', AuthController.logInUser)
 
-router.post('/login', AuthController.logInUser);
+router.post('/logout', AuthController.logOutUser)
 
-router.post('/logout', AuthController.logOutUser);
-
-router.get('/me', AuthController.getCurrentUser);
+router.get('/me', AuthController.getCurrentUser)
 
 module.exports = router;
 
