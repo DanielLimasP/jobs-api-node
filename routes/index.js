0c928d6160bdd3d4242962b568b3a33cf15a7511
FranciscoJRamirez <42820012+FranciscoJRamirez@users.noreply.github.com>
2020-05-19 01:27:06 -0600

    function delete and update users with some mistakes (incompletes)

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,46 @@ router.get('/globalusers', async (req, res)=>{
                     _id: concept._id,
                     name: concept.profile.name,
                     lastname: concept.profile.lastname,
-                    email: concept.profile.email
-                    
+                    email: concept.profile.email,
+                    password: concept.profile.password,
+                    phone: concept.profile.phone
                 }
             })
         }
-        console.log("Body", ctx.users)
+        
         res.render('usersviews/globalusers', {users: ctx.users})
     })
     
+})
+
+router.get('/edituser/:id', async (req, res)=>{
+    const UserId = await UserModel.findById(req.params.id)
+    const user = {
+        _id: UserId._id,
+        name: UserId.profile.name,
+        lastname: UserId.profile.lastname,
+        email: UserId.profile.email,
+        password: UserId.profile.password,
+        phone:  UserId.profile.phone
+    }
+    res.render('usersviews/edituser', {user})
+})
+
+router.put('/edit/:id', async (req, res)=>{
+    const { name, lastname, email, password, phone } = req.body
+    console.log('id', req.params.id)
+    console.log('body', req.body)
+    console.log('variables', {name, lastname, email, password, phone})
+    await UserModel.findByIdAndUpdate(req.params.id, {name, lastname, email, password, phone})
+
+    req.flash('success_msg', 'User uptdated Successfully')
+    res.status(200).redirect('/globalusers')
+})
+
+router.delete('/deleteuser/:id', async (req, res)=>{
+    await UserModel.findByIdAndDelete(req.params._id)
+    req.flash('success_msg', 'User Removed Successfully')
+    res.redirect('/usersviews/globalusers')
 })
 
 module.exports = router
